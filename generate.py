95011686c7b5073abfa8951be22569d17540c044
hau.tran <hau.tran@ubitec.ch>
2022-01-04 13:26:33 +0700

    Add support for specifying `port` for each `site` instead of 443
    This is to prepare for monitoring a few sites that do not use the
    conventional 443 port.

--- generate.py
+++ generate.py
@@ -9,12 +9,12 @@ def main():
     with open('sites.yaml', 'r') as sites_yaml:
         configuration = yaml.safe_load(sites_yaml)
     
-    domains = [site['domain'] for site in configuration['sites']]
+    sites = [site for site in configuration['sites']]
     with open('tls_monitor_template.yaml', 'r') as template:
         mustache_template = template.read()
-        for domain in domains:
-            with open(f".github/workflows/{domain}.yaml", 'w') as workflow:
-                content = chevron.render(mustache_template, { 'domain': domain })
+        for site in sites:
+            with open(f".github/workflows/{site['domain']}.yaml", 'w') as workflow:
+                content = chevron.render(mustache_template, site) 
                 workflow.write(content)
 
 if __name__ == '__main__':
